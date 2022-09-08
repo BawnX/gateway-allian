d3b84a6e862f6be7515aca1d33d41f9341aad51f
Antonio Morales <antonio.morales@anschile.cl>
2022-09-08 13:30:28 -0300

    fix: fixed problem with import

--- server.mjs
+++ server.mjs
@@ -1,20 +1,20 @@
-const { ApolloServer } = require('apollo-server');
-const { ApolloGateway, IntrospectAndCompose } = require('@apollo/gateway');
+import {ApolloServer} from 'apollo-server';
+import {ApolloGateway, IntrospectAndCompose} from '@apollo/gateway';
 
 const gateway = new ApolloGateway({
-  supergraphSdl: new IntrospectAndCompose({
-    subgraphs: [
-      { name: 'users', url: 'http://localhost:4001/query' },
-      { name: 'profile', url: 'http://localhost:4002/query' },
-    ],
-  })
+    supergraphSdl: new IntrospectAndCompose({
+        subgraphs: [
+            {name: 'users', url: 'http://localhost:4001/query'},
+            {name: 'profile', url: 'http://localhost:4002/query'},
+        ],
+    })
 });
 
 const server = new ApolloServer({
-  gateway,
-  subscriptions: false,
+    gateway,
+    subscriptions: false,
 });
 
-server.listen(process.env.PORT).then(({ url }) => {
-  console.log(`Server ready at ${url}`);
+server.listen(process.env.PORT).then(({url}) => {
+    console.log(`Server ready at ${url}`);
 });
